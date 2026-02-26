a4625e07247fdd48b1700c267e52d1e3b674e322
fbrouille <fbrouille@users.noreply.github.com>
2026-02-26 19:06:20 +0000

    Add gio::subclass::File::file_create_async/finish
    Signed-off-by: fbrouille <fbrouille@users.noreply.github.com>

--- gio/src/subclass/file.rs
+++ gio/src/subclass/file.rs
@@ -244,6 +244,16 @@ pub trait FileImpl: ObjectImpl + ObjectSubclass<Type: IsA<File>> {
         self.parent_create(flags, cancellable)
     }
 
+    fn create_future(
+        &self,
+        flags: FileCreateFlags,
+        priority: glib::Priority,
+    ) -> std::pin::Pin<
+        Box<dyn std::future::Future<Output = Result<FileOutputStream, Error>> + 'static>,
+    > {
+        self.parent_create_future(flags, priority)
+    }
+
     fn replace(
         &self,
         etag: Option<&str>,
@@ -1595,6 +1605,57 @@ pub trait FileImplExt: FileImpl {
         }
     }
 
+    fn parent_create_async<R: FnOnce(Result<FileOutputStream, Error>) + 'static>(
+        &self,
+        flags: FileCreateFlags,
+        priority: glib::Priority,
+        cancellable: Option<&Cancellable>,
+        callback: R,
+    ) {
+        assert_async_is_allowed();
+        unsafe {
+            let type_data = Self::type_data();
+            let parent_iface =
+                type_data.as_ref().parent_interface::<File>() as *const ffi::GFileIface;
+
+            let f = (*parent_iface)
+                .create_async
+                .expect("no parent interface implementation for \"create_async\"");
+            let finish = (*parent_iface)
+                .create_finish
+                .expect("no parent interface \"create_finish\" implementation");
+
+            let user_data: Box<(glib::thread_guard::ThreadGuard<R>, _)> =
+                Box::new((glib::thread_guard::ThreadGuard::new(callback), finish));
+
+            f(
+                self.obj().unsafe_cast_ref::<File>().to_glib_none().0,
+                flags.into_glib(),
+                priority.into_glib(),
+                cancellable.to_glib_none().0,
+                Some(file_async_trampoline::<FileOutputStream, R>),
+                Box::into_raw(user_data) as *mut _,
+            );
+        }
+    }
+
+    fn parent_create_future(
+        &self,
+        flags: FileCreateFlags,
+        priority: glib::Priority,
+    ) -> std::pin::Pin<
+        Box<dyn std::future::Future<Output = Result<FileOutputStream, Error>> + 'static>,
+    > {
+        Box::pin(GioFuture::new(
+            &self.ref_counted(),
+            move |imp, cancellable, send| {
+                imp.parent_create_async(flags, priority, Some(cancellable), move |res| {
+                    send.resolve(res);
+                });
+            },
+        ))
+    }
+
     fn parent_replace(
         &self,
         etag: Option<&str>,
@@ -2938,6 +2999,8 @@ unsafe impl<T: FileImpl> IsImplementable<T> for File {
         iface.append_to_async = Some(file_append_to_async::<T>);
         iface.append_to_finish = Some(file_append_to_finish);
         iface.create = Some(file_create::<T>);
+        iface.create_async = Some(file_create_async::<T>);
+        iface.create_finish = Some(file_create_finish);
         iface.replace = Some(file_replace::<T>);
         iface.delete_file = Some(file_delete_file::<T>);
         iface.trash = Some(file_trash::<T>);
@@ -2982,8 +3045,6 @@ unsafe impl<T: FileImpl> IsImplementable<T> for File {
         // iface._query_settable_attributes_finish = Some(_file_query_settable_attributes_finish::<T>);
         // iface._query_writable_namespaces_async = Some(_file_query_writable_namespaces_async::<T>);
         // iface._query_writable_namespaces_finish = Some(_file_query_writable_namespaces_finish::<T>);
-        // iface.create_async = Some(file_create_async::<T>);
-        // iface.create_finish = Some(file_create_finish::<T>);
         // iface.replace_async = Some(file_replace_async::<T>);
         // iface.replace_finish = Some(file_replace_finish::<T>);
         // iface.delete_file_async = Some(file_delete_file_async::<T>);
@@ -3866,6 +3927,36 @@ unsafe extern "C" fn file_create<T: FileImpl>(
             }
         }
     }
+}
+
+unsafe extern "C" fn file_create_async<T: FileImpl>(
+    file: *mut ffi::GFile,
+    flags: ffi::GFileCreateFlags,
+    io_priority: i32,
+    cancellable: *mut ffi::GCancellable,
+    callback: ffi::GAsyncReadyCallback,
+    user_data: glib::ffi::gpointer,
+) {
+    unsafe {
+        let instance = &*(file as *mut T::Instance);
+        let imp = instance.imp();
+
+        spawn_async_with_local_task(
+            file,
+            cancellable,
+            callback,
+            user_data,
+            imp.create_future(from_glib(flags), from_glib(io_priority)),
+        );
+    }
+}
+
+unsafe extern "C" fn file_create_finish(
+    _file: *mut ffi::GFile,
+    res_ptr: *mut ffi::GAsyncResult,
+    error_ptr: *mut *mut glib::ffi::GError,
+) -> *mut ffi::GFileOutputStream {
+    unsafe { finish_local_task::<FileOutputStream>(res_ptr, error_ptr) }
 }
 
 unsafe extern "C" fn file_replace<T: FileImpl>(
